a95330d7ab69dc95b1d0112be482a05814e6b3f4
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-04-25 02:50:49 +0500

    fixed

--- src/pages/components/Tasks.js
+++ src/pages/components/Tasks.js
@@ -3,18 +3,11 @@ import axios from "axios"
 
 export default function Task({task, reloadTask}) {
 
-    const [updateTask, setUpdateTask] = React.useState("")
 
     const deleteTodo = () => {
-        
-        axios.post("/api/delete-task", {id:task._id})
-        .then(reloadTask)
+        axios.post("/api/delete-task", {id:task._id}).then(reloadTask)
     }
-    const updateTodo = () => {
-        axios.post("/api/update-task",{id: task._id, task: updateTask}).then(reloadTask)
-        if(task._id === updateTask._id)return
-
-    }
+  
 
     return (
         <div>
