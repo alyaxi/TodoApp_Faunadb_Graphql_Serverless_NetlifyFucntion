fe899dde75147d19613f48ebc1500803d9c65adc
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-04-28 01:17:30 +0500

    add netlify authentication

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+      // You should only have one instance of this plugin
+      {
+        resolve: `gatsby-plugin-netlify-identity`,
+        options: {
+          url: `https://todoapp21.netlify.app/` // required!
+        }
+      }
+    ]
+  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,14 @@ import axios from "axios";
 import React, { useState } from "react"
 import Form from "./components/Form";
 import Task from "./components/Tasks";
+import IdentityModal, { useIdentityContext } from "react-netlify-identity-widget"
+import "react-netlify-identity-widget/styles.css"
 
 
 const Index = () => {
+
+    // Fetch data and display over UI
+
     const [status, setStatusCode] = useState("loading");
     const [task, setTask] = useState(null)
     React.useEffect(() => {
@@ -30,10 +35,25 @@ const Index = () => {
     fetchTodo()
     }, [status])
     const reloadTask = () => {setStatusCode("loading")}
+
+    //Login Setup
+
+    const identity = useIdentityContext() // see https://github.com/sw-yx/react-netlify-identity for api of this identity object
+    const [dialog, setDialog] = React.useState(false)
+    const name =
+    (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name) || "NoName"
+  const isLoggedIn = identity && identity.isLoggedIn
+
+    
     return(
         <main>
             <h1>Todo App</h1>
-            <Form reloadTask={reloadTask}/>
+            {identity && identity.isLoggedIn ? (
+                <>
+                <button onClick={() => {setDialog(true)}}>
+                 {isLoggedIn ? `Hello ${name} Logout!` : `Login`}
+                </button>
+                <Form reloadTask={reloadTask}/>
             {task ? (
                 <ul>
                     {task.map(task => {
@@ -47,6 +67,15 @@ const Index = () => {
                     })}
                 </ul>
             ): (<p>Loading...</p>)}
+                </>
+            ): (
+                <button onClick={() => setDialog(true)}>
+                    {isLoggedIn ? `Hello ${name}, Logout`: `Login`}
+                </button>
+            
+            )}
+        <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
+            
         </main>
     )
 }
