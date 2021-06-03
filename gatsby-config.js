db9b08eaff587d388c5152676f1b65c944bef72c
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-06-04 02:41:38 +0500

    added command22

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
     plugins: [
       // You should only have one instance of this plugin
-      {
-        resolve: `gatsby-plugin-netlify-identity`,
-        options: {
-          url: `https://todoapp212.netlify.app/` // required!
-        }
-      }
+      // {
+      //   resolve: `gatsby-plugin-netlify-identity`,
+      //   options: {
+      //     url: `https://todoapp212.netlify.app/` // required!
+      //   }
+      // }
     ]
   }
